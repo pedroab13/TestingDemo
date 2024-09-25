2ce66b2ba3319ebca55184f6db3975c7d66d9b9a
Pedro Álvarez <Pedro lvarez>
2024-09-25 11:05:07 +0200

    Finished customer class

--- MyLibrary/Customer.cs
+++ MyLibrary/Customer.cs
@@ -1,6 +1,11 @@
 ﻿namespace MyLibrary
 {
-    class Customer
+    public class Customer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public String FullName => $"{FirstName} {LastName}";
     }
-}
+ }
+
